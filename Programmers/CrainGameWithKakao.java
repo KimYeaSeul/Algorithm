b06a391e5ada7fa797fe0a2671381860aba54380
KimYeaSeul <adgjl3151@hanmail.net>
2022-05-16 13:51:30 +0900

    main 함수 만들어 놓기. 다음은 테스트할수 있게 만들어보기

--- Programmers/CrainGameWithKakao.java
+++ Programmers/CrainGameWithKakao.java
@@ -1,5 +1,14 @@
 import java.util.*;
-class Solution {
+class CrainGameWithKakao {
+
+    public static void main(String[] args){
+        CrainGameWithKakao cgwk = new CrainGameWithKakao();
+        int[][] board = {{0,0,0,0,0},{0,0,1,0,3},{0,2,5,0,1},{4,2,4,4,2},{3,5,1,3,1}};
+        int[] moves = {1,5,3,5,1,2,1,4};
+        // 4
+        System.out.println(cgwk.solution(board, moves));
+    }
+
     public int solution(int[][] board, int[] moves) {
         int answer = 0;
         int boardLength = board.length;
