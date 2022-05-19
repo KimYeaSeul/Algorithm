95a728f4d4c1a867ef8e881c1f5228e4dc9a982b
KimYeaSeul <adgjl3151@hanmail.net>
2022-05-19 16:31:04 +0900

    중복은 함수로 빼기, 문자열 합치기에 join쓰기

--- Programmers/KeyPad.java
+++ Programmers/KeyPad.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 class KeyPad {
   public static void main(String[] args) {
     KeyPad kp = new KeyPad();
@@ -7,39 +9,37 @@ class KeyPad {
     System.out.println(kp.solution(numbers, hand));
   }
 
-  public String solution(int[] numbers, String hand) {
-    String answer = "";
-    int[][] keyPad = { { 3, 1 }, { 0, 0 }, { 0, 1 }, { 0, 2 }, { 1, 0 }, { 1, 1 }, { 1, 2 }, { 2, 0 }, { 2, 1 },
-        { 2, 2 } };
-    int[] L = { 3, 0 };
-    int[] R = { 3, 2 };
+  int[][] keyPad = {{3,1},{0,0},{0,1},{0,2},{1,0},{1,1},{1,2},{2,0},{2,1},{2,2}};
+  int[] L = {3,0};
+  int[] R = {3,2};
+  ArrayList<String> list = new ArrayList<>();
 
-    for (int n : numbers) {
-      int row = keyPad[n][1];
-      if (row == 0) {
-        L = keyPad[n];
-        answer += "L";
-      } else if (row == 2) {
-        R = keyPad[n];
-        answer += "R";
-      } else if (row == 1) {
-        int lll = Math.abs(keyPad[n][0] - L[0]) + Math.abs(keyPad[n][1] - L[1]);
-        int rrr = Math.abs(keyPad[n][0] - R[0]) + Math.abs(keyPad[n][1] - R[1]);
-        if (lll > rrr) {
-          R = keyPad[n];
-          answer += "R";
-        } else if (lll < rrr) {
-          L = keyPad[n];
-          answer += "L";
-        } else if (lll == rrr && hand.equals("left")) {
-          L = keyPad[n];
-          answer += "L";
-        } else if (lll == rrr && hand.equals("right")) {
-          R = keyPad[n];
-          answer += "R";
+  public String solution(int[] numbers, String hand) {
+        
+    for(int n : numbers){
+        int row = keyPad[n][1];
+        if(row==0) finger("L", n);
+        else if(row==2) finger("R", n);
+        else if(row==1){
+            int lll = distance(n, L);
+            int rrr = distance(n, R);
+            if(lll < rrr || (lll == rrr && hand.equals("left"))){
+                finger("L", n);
+            }else if(lll > rrr || (lll == rrr && hand.equals("right"))){
+                finger("R", n);
+            }
         }
-      }
     }
-    return answer;
+    return String.join("",list);
+  }
+
+  public void finger(String thum, int n){
+      if(thum.equals("R")) R=keyPad[n];
+      else if(thum.equals("L")) L=keyPad[n];
+      list.add(thum);
+  }
+
+  public int distance(int n, int[] thum){
+      return Math.abs(keyPad[n][0]-thum[0]) + Math.abs(keyPad[n][1]-thum[1]);
   }
 }
