0e5ccac34ab06b8ee28080c95064b806a64ff68f
KimYeaSeul <adgjl3151@hanmail.net>
2022-06-25 01:42:45 +0900

    String.join()으로 문자열 합치기

--- Programmers/BigNumber.java
+++ Programmers/BigNumber.java
@@ -13,21 +13,21 @@ public class BigNumber {
   public String solution(int[] numbers) {
     String answer = "";
     
-    List<Integer> listInt = new ArrayList<>();
-    for(int bb : numbers) listInt.add(bb);
-    Collections.sort(listInt, new Comparator<Integer>() {
+    List<String> listInt = new ArrayList<>();
+    for(int bb : numbers) listInt.add(String.valueOf(bb));
+    Collections.sort(listInt, new Comparator<String>() {
 
       @Override
-      public int compare(Integer o1, Integer o2) {
-        String v1 = "" + o1 + o2;
-        String v2 = "" + o2 + o1;
+      public int compare(String o1, String o2) {
+        String v1 = o1 + o2;
+        String v2 = o2 + o1;
         int v = Integer.parseInt(v2)-Integer.parseInt(v1);
         return v ;
       }
       
     });
       
-    for(int n : listInt){ answer += n; }
+    answer = String.join("", listInt);
     // 0 으로 시작한다는건 뒤에가 다 0이라는거지
     // 0보다 큰수가 있으면 맨 앞으로 왔을테니까!
     if(answer.startsWith("0"));
