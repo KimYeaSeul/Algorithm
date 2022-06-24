35ea5ca10be10cde5f8262ff542a5cc256049a13
KimYeaSeul <adgjl3151@hanmail.net>
2022-06-25 01:36:08 +0900

    Lv2. 조이스틱 : Greedy탐욕법

---
+++ Programmers/JoyStick.java
@@ -0,0 +1,28 @@
+public class JoyStick {
+
+  public static void main(String[] args) {
+    JoyStick js = new JoyStick();
+    String name = "JEROEN";
+    // 56
+    System.out.println(js.solution(name));
+  }
+  public int solution(String name){
+    int answer = 0;
+        int length = name.length();
+
+        int index;
+        int move = length - 1;
+
+        for(int i = 0; i < name.length(); i++){
+            answer += Math.min(name.charAt(i) - 'A', 'Z' - name.charAt(i) + 1);
+
+            index = i + 1;
+            while(index < length && name.charAt(index) == 'A'){
+                index++;
+            }
+            move = Math.min(move, i * 2 + length - index);
+            move = Math.min(move, (length - index) * 2 + i);
+        }
+        return answer + move;
+  }
+}
