e7a8c16afbc694a64596c76ad40dc0e9424a4eea
KimYeaSeul <adgjl3151@hanmail.net>
2022-06-25 01:40:49 +0900

    Lv2. 가장 큰 수 : Collection.sort(List, Comparator)

--- Programmers/BigNumber.java
+++ Programmers/BigNumber.java
@@ -12,28 +12,25 @@ public class BigNumber {
   }
   public String solution(int[] numbers) {
     String answer = "";
-    Arrays.sort(numbers);
-    // sort는 아니다.그렇다고 한글자씩 떼서 볼 수도 없다.
-    Integer[] refInt = new Integer[numbers.length];
-    for(int i =0; i<numbers.length; i++){
-      refInt[i] = numbers[i];
-    }
-    Arrays.sort(refInt, new Comparator<Integer>() {
-      // Override된 compare 함수를 어떻게 정의하냐에 따라서 다양한 정렬이 가능해집니다
+    
+    List<Integer> listInt = new ArrayList<>();
+    for(int bb : numbers) listInt.add(bb);
+    Collections.sort(listInt, new Comparator<Integer>() {
+
       @Override
       public int compare(Integer o1, Integer o2) {
-          String v1 = "" + o1 + o2;
-            String v2 = "" + o2 + o1;
-            int v = Integer.parseInt(v2)-Integer.parseInt(v1);
-          // System.out.println(o1 + " - "+ o2 + " = "+v  );
-          return v;
+        String v1 = "" + o1 + o2;
+        String v2 = "" + o2 + o1;
+        int v = Integer.parseInt(v2)-Integer.parseInt(v1);
+        return v ;
       }
+      
     });
       
-      for(int n : refInt){
-        answer += n;
-      }
-      if(answer.startsWith("0"));
+    for(int n : listInt){ answer += n; }
+    // 0 으로 시작한다는건 뒤에가 다 0이라는거지
+    // 0보다 큰수가 있으면 맨 앞으로 왔을테니까!
+    if(answer.startsWith("0"));
     return answer;
   }
 }
