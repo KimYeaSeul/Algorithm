2d58beab0612b5a857e7f1b640e205d67d4665db
KimYeaSeul <adgjl3151@hanmail.net>
2022-09-13 23:15:27 +0900

    fix : using lambda

--- src/main/java/programmers/Lv1_1.java
+++ src/main/java/programmers/Lv1_1.java
@@ -11,18 +11,19 @@ public class Lv1_1 {
     // https://school.programmers.co.kr/learn/courses/30/lessons/12916
     public boolean _문자열_내_p와_y의_개수(String s){
         String lowS = s.toLowerCase();
-        int result = 0;
-        if(!lowS.contains("p")&&!lowS.contains("y")){
-            return true;
-        }
-        for(int i=0; i<s.length();i++){
-            if(lowS.charAt(i) == 'p'){
-                result++;
-            }else if(lowS.charAt(i) == 'y'){
-                result--;
-            }
-        }
-
-        return result == 0? true : false;
+//        int result = 0;
+//        if(!lowS.contains("p")&&!lowS.contains("y")){
+//            return true;
+//        }
+//        for(int i=0; i<s.length();i++){
+//            if(lowS.charAt(i) == 'p'){
+//                result++;
+//            }else if(lowS.charAt(i) == 'y'){
+//                result--;
+//            }
+//        }
+//
+//        return result == 0;
+        return lowS.chars().filter(e -> 'p'==e).count() == lowS.chars().filter(e -> 'y'==e).count();
     }
 }
