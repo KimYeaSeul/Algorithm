340df743b9b2242eebc5d2e0a8e48b89d3bfe3e5
KimYeaSeul <adgjl3151@hanmail.net>
2022-09-13 23:00:27 +0900

    Lv1.문자열 내 p와 y의 개수

--- src/main/java/programmers/Lv1_1.java
+++ src/main/java/programmers/Lv1_1.java
@@ -7,4 +7,22 @@ public class Lv1_1 {
     public String _짝수와_홀수(int num){
         return num%2==0? "Even" : "Odd";
     }
+
+    // https://school.programmers.co.kr/learn/courses/30/lessons/12916
+    public boolean _문자열_내_p와_y의_개수(String s){
+        String lowS = s.toLowerCase();
+        int result = 0;
+        if(!lowS.contains("p")&&!lowS.contains("y")){
+            return true;
+        }
+        for(int i=0; i<s.length();i++){
+            if(lowS.charAt(i) == 'p'){
+                result++;
+            }else if(lowS.charAt(i) == 'y'){
+                result--;
+            }
+        }
+
+        return result == 0? true : false;
+    }
 }
--- src/test/java/programmers/Lv1_Test.java
+++ src/test/java/programmers/Lv1_Test.java
@@ -6,11 +6,17 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class Lv1_Test {
 
-    private Lv1_1 lv1 = new Lv1_1();
+    private final Lv1_1 lv1 = new Lv1_1();
 
     @Test
     void _짝수와_홀수() {
         assertEquals("Even", lv1._짝수와_홀수(4));
         assertEquals("Odd", lv1._짝수와_홀수(3));
+    }
+
+    @Test
+    void _문자열_내_p와_y의_개수() {
+        assertTrue(lv1._문자열_내_p와_y의_개수("pPoooyY"));
+        assertFalse(lv1._문자열_내_p와_y의_개수("Pyy"));
     }
 }
